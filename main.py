98af2d239f1873c2bf612f5aa698d0bbf053f156
Luker <luker.plus@gmail.com>
2020-04-06 22:49:41 +0200

    descriptions

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def display(checkpoint, nazwa, contour):
 
     return ploty
 
-
+#znajdywacz podstawy drugą metodą, nie istotne
 def find_base2(img):
     data = canny(img, sigma=3)
     data = dilation(data)
@@ -43,6 +43,7 @@ def find_base2(img):
             base_line = line
     return base_line
 
+#znajdywacz podstawy pierwsza metodą, nie istotne
 def find_base(img):
     contours = find_contours(img, 0)
     max_distance = 0
@@ -55,38 +56,51 @@ def find_base(img):
             base_line = [[coords[i+1][1],coords[i+1][0]],[coords[i][1],coords[i][0]]]
     return base_line
 
+#znajduje prostopadłe do prostej przechodzącej przez punkty z inputu, proste są zapisane jako a oraz b
 def find_perpendicular(points):
     if ((points[1][0] - points[0][0]) == 0 or (points[1][1] - points[0][1]) == 0):
         slope = 0
     else:
         slope = (points[1][1] - points[0][1]) / (points[1][0] - points[0][0])
         slope = -1/slope
+    #slope to a w funkcji liniowej
+
+    #to jest przygotowanko do generowania punktów interpolowanych pomiędzy tymi dwoma punktami z inputu
+    pointNum = 20
     diff_X = points[1][0]- points[0][0]
     diff_Y = points[1][1] - points[0][1]
-    pointNum = 20
-
     interval_X = diff_X / (pointNum + 1)
     interval_Y = diff_Y / (pointNum + 1)
 
     point_list = []
     lines = []
+    #tu generuje punkty pomiędzy tymi punktami z inputu (czyli punkty na podstawie) i proste prostopadłe, które
+    #przez nie przechodzą i po których znajdziemy te punkty z przecięcia
     for i in range(1,pointNum+1):
         point_list.append([points[0][0] + interval_X * i, points[0][1] + interval_Y*i])
         lines.append([slope, point_list[i - 1][1] - (slope * point_list[i - 1][0])])
     return lines, point_list
 
-
+#sprawdzam po której stronie lini jest figura
 def orientation_check(img, line, point):
-    y = math.floor(line[0] * (math.floor(point[0])+2) + line[1])
-    if y>len(img) or y<0 or math.floor(point[0])+2>len(img[0]):
+    # to plus 3 zapewnia, że na pewno już się zaczęła ta figura, bo te punkty mogą być czasami lekko oddalone od figury
+    # ale można w sumie zwiększyć jak nie będzie łapać
+    y = math.floor(line[0] * (math.floor(point[0])+3) + line[1])
+    if y>len(img) or y<0 or math.floor(point[0])+3>len(img[0]):
         return 0
     if img[y][math.floor(point[0])+1] > 0:
         return 1
     else:
         return 0
 
+
+# to jest w sumie skomplikowaność, lines to te prostopadłe do podstawy linie, a points to punkty na podstawie,
+# przez które te proste przechodzą
+
+# w skrócie od punktu podstawy ide po odpowiadającej mu funkcji liniowej aż nie skończy się obrazek
 def find_furthest_point(img, lines,points):
     result = []
+    #orientacje sprawdzam na prostopadłej przechodzącej przez mniej więcej środek podstawy
     orientation = orientation_check(img, lines[math.floor(len(lines)/2)],points[math.floor(len(lines)/2)])
     for n in range(len(lines)):
         line = lines[n]
@@ -94,11 +108,13 @@ def find_furthest_point(img, lines,points):
             for_range = range(math.floor(points[n][0])-1,0, -1)
         else:
             for_range = range(math.ceil(points[n][0])+1, len(img[0]), 1)
+        # dla każdej prostej jadę po niej od punktu aż do końca figury
         for i in for_range:
             if orientation!=1:
                 y = math.floor(line[0]*i+line[1])
             else:
                 y = math.ceil(line[0] * i + line[1])
+            #jak się kończy ten biały obrazek, albo fizycznie obrazek to uznaje, że dotarłem do końca
             if y>len(img) or y<0:
                 result.append([i, y])
                 break
